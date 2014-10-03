c39351caa240115d5347bb809c378f668d274efe
Ben Meline <benjaminmeline2015@u.northwestern.edu>
2014-10-03 11:03:47 -0500

    Refactor

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,26 @@
 class EventController < ApplicationController
 	require_dependency 'event_page_parser'
+
 	def show
-		url = params[:url]
-		@res = EventPageParser::ImageScraper.links(url)
+		@url = params[:url]
+		if valid
+			@res = links
+			render :show
+		else
+			render :fail
+		end
+	end
+
+	private
+	def image_scraper
+		@image_scraper ||= EventPageParser::ImageScraper.new
+	end
+
+	def valid
+		image_scraper.valid(@url)
+	end
+
+	def links
+		image_scraper.links(@url)
 	end
 end
